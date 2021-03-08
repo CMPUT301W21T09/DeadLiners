9363709b9c7a10cff75930302e92ca8a158dcae1
Zhijian-Mei <46204799+Zhijian-Mei@users.noreply.github.com>
2021-03-08 21:46:17 +0800

    experiment's layout
    experiment's internal layout, and experiment's layout in listview.

--- projectFile/app/src/main/java/com/example/myapplication/MainActivity.java
+++ projectFile/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
