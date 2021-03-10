0e9b56faf858037fcc4ef0c00b2a07d9e3f211f1
Zhuoyan Li <zhuoyan1@ualberta.ca>
2021-03-10 08:22:17 +0800

    basic structure of Reply,Question,User && question,reply's layout

--- projectFile/app/src/main/java/com/example/myapplication/Question.java
+++ projectFile/app/src/main/java/com/example/myapplication/Question.java
@@ -1,6 +1,30 @@
 package com.example.myapplication;
 
+import android.location.Location;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
 public class Question {
+    private String description;
+    private User owner;
+    private Date time;
+    private ArrayList<Reply> replies;
 
+    public Question(String description, Date time) {
+        this.description = description;
+        this.time = time;
+    }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public ArrayList<Reply> getReplies() {
+        return replies;
+    }
 }
