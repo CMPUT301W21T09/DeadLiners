9cc95847dca7caf63fcd88c0211c129ba10428e7
Haoyang-Lin <78832633+Haoyang-Lin@users.noreply.github.com>
2021-03-13 02:41:36 +0800

    Fix the problem of login button

--- projectFile/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ projectFile/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.app.AppComponentFactory;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,20 +20,22 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
-    public ArrayList<User> userArrayList;
-    public ArrayList<String> uidList;
+    public ArrayList<User> userArrayList = new ArrayList<User>();
+    public ArrayList<String> uidList = new ArrayList<String>();
     String uid;
     private FirebaseFirestore db = FirebaseFirestore.getInstance() ;
     CollectionReference userCollectionReference = db.collection("Users");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        userArrayList = new ArrayList<User>();
+        //userArrayList = new ArrayList<User>();
         userCollectionReference.get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -41,8 +44,8 @@ public class LoginActivity extends AppCompatActivity {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d("DOC", document.getId() + " => " + document.getData());
                                 //Toast.makeText(LoginActivity.this,document.getId() + "=> " + document.getData(),Toast.LENGTH_SHORT).show();
-                                User user = new User(document.getId());
                                 uidList.add(document.getId());
+                                //Toast.makeText(LoginActivity.this, uidList.size(),Toast.LENGTH_SHORT).show();
                             }
                         } else {
                             Log.d("DOC", "Error getting documents: ", task.getException());
@@ -53,7 +56,16 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.log_in_activity);
         Button OK = (Button)findViewById(R.id.login);
         EditText user = (EditText)findViewById(R.id.uid);
-
+        for (int i=0; i < uidList.size(); i++){
+            Toast.makeText(LoginActivity.this, uidList.get(i),Toast.LENGTH_SHORT).show();
+        }
+        if(uidList.isEmpty()){
+            user.setText("u001");
+        }else{
+            String str = null;
+            str = String.format("u%03d",uidList.size()+1);
+            user.setText(str);
+        }
         OK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,6 +73,14 @@ public class LoginActivity extends AppCompatActivity {
                 uid = user.getText().toString();
                 intent.putExtra("return", uid);
                 setResult(1,intent);
+                HashMap<String,String> email = new HashMap<>();
+                HashMap<String,String> username = new HashMap<>();
+                email.put("Email","Default Email");
+                username.put("Username","default Username");
+                userCollectionReference.document(uid)
+                        .set(email);
+                userCollectionReference.document(uid)
+                        .set(username, SetOptions.merge());
                 finish();
             }
         });
