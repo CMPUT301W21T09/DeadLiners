3a5852a7a72cd5224e89980599da01bb0d47c1ce
Zhijian-Mei <46204799+Zhijian-Mei@users.noreply.github.com>
2021-04-09 12:21:50 +0800

    fix

--- projectFile/app/src/main/java/com/example/myapplication/experimentInfo_owner.java
+++ projectFile/app/src/main/java/com/example/myapplication/experimentInfo_owner.java
@@ -88,6 +88,7 @@ public class experimentInfo_owner extends AppCompatActivity {
     private Button unPublish;
     private Button end;
     private Button barCode;
+    private Button seeMap;
 
     private TextView experimentName;
     private TextView description;
@@ -125,6 +126,11 @@ public class experimentInfo_owner extends AppCompatActivity {
         end = findViewById(R.id.End);
         aSwitch = findViewById(R.id.Geo_enable);
         barCode = findViewById(R.id.barcode);
+        seeMap = findViewById(R.id.seeMap_owner);
+
+        if (experiment.getGeoState().equals("0")){
+            seeMap.setVisibility(View.INVISIBLE);
+        }
 
         if (experiment.getGeoState().equals("1")) {
             aSwitch.setChecked(true);
@@ -140,6 +146,8 @@ public class experimentInfo_owner extends AppCompatActivity {
         status.setText(experiment.getPublished());
 
         String expName = experiment.getExpName();
+
+
 
         viewTrails.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -157,9 +165,6 @@ public class experimentInfo_owner extends AppCompatActivity {
         addTrail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (experiment.getGeoState().equals("1")) {
-                    Toast.makeText(experimentInfo_owner.this,"This experiment require your location!",Toast.LENGTH_SHORT).show();
-                }
                 if (experiment.getCategory().equals("count") && (experiment.getPublished().equals("open"))){
                     String currentTime = String.format("%d",currentTimeMillis());
 
@@ -565,7 +570,7 @@ public class experimentInfo_owner extends AppCompatActivity {
                         fusedLocationProviderClient.requestLocationUpdates(locationRequest
                         ,locationCallback, Looper.myLooper());
                     }
-                    
+
                     HashMap<String, Double> loc = new HashMap<>();
                     loc.put("longi", longitude);
                     loc.put("lat", latitude);
